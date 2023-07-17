bd786da8b832e98066f0473417ee44fd17d97da7
orta <git@orta.io>
2023-07-17 12:04:21 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { ExtractPublicPropTypes } from 'vue';
+import IframeRendererVue from './components/IframeRenderer.vue';
+
+// type IframeRendererProps = ExtractPublicPropTypes<InstanceType<typeof IframeRendererVue>["$props"]>
+const params = new URLSearchParams(document.location.search)
+const props = {
+  defaultUrl: params.get("url") || "http://localhost:5678/?embedID=orta-test",
+  defaultWidth: params.get("width") || "640px",
+  defaultHeight: params.get("height") || "480px"
+  
+}
+</script>
+
+<template>
+  <main>
+    <h1>iframe tester</h1>
+    <h3>Enter a URL to set as the iframe, all possible params are saved into the URL so you can refresh/share.</h3>
+    <IframeRendererVue v-bind="props" />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+h3 {
+  margin-bottom: 24px;
+}
+
+main {
+  margin: 0 auto;
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/IframeRenderer.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const props = defineProps<{
+  defaultUrl: string
+  defaultWidth: string
+  defaultHeight: string
+}>()
+let url = ref(props.defaultUrl)
+let width = ref(props.defaultWidth)
+let height = ref(props.defaultHeight)
+let otherAttributes = ref({} as Record<string, string>)
+
+
+
+const updateURL = () => {
+  const params = new URLSearchParams()
+  params.set("url", url.value)
+  params.set("width", width.value)
+  params.set("height", height.value)
+  params.set("otherAttributes", JSON.stringify(otherAttributes.value))
+  document.location.search = params.toString()
+}
+
+watch(url, updateURL)
+watch(width, updateURL)
+watch(height, updateURL)
+watch(otherAttributes, updateURL)
+
+const iframeAttributes = ["allow", "csp", "importance", "loading", "name", "referrerpolicy", "sandbox", "srcdoc"]
+
+</script>
+
+<template>
+  <input id="url" v-model="url" placeholder="URL here" />
+  <iframe :width="width" :height="height" :src="url" v-bind="otherAttributes"></iframe>
+
+  <div>
+    <p>Size:</p>
+    <input v-model="width" placeholder="640px" />
+    by
+    <input v-model="height" placeholder="480px" />
+  </div>
+
+  <h3>Iframe custom Attributes:</h3>
+
+  <div class="attributes">
+    <ul>
+      <li v-for="value in iframeAttributes">
+        <p><code><a :href="`https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#${value}`">{{ value }}</a></code></p>
+        <input v-model="otherAttributes[value]" placeholder="" />
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  margin-top: 50px;
+  margin-bottom: 0px;
+}
+
+iframe {
+  margin-top: 12px;
+  margin-bottom: 36px;
+}
+
+input#url {
+  font-size: x-large;
+  width: 100%;
+}
+
+div {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
