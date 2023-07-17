1b7c8284d1dd9f5429f94c56af724ea53b333336
orta <git@orta.io>
2023-07-17 12:20:37 +0100

    Adds CI folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.CI ? "/iframe-tester" : undefined,
   plugins: [
     vue(),
     vueJsx(),
